f139543ada58cb5cb6cd81d85e3590905fd4809b
Márk Fazekas <coldrex.fazekas.mark@gmail.com>
2021-11-13 12:21:12 +0100

    Solution

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,11 @@
 
+const setCookie = (value, key = "token") => {
+    const saveDate = new Date();
+    saveDate.setMinutes(saveDate.getMinutes() + 15);
+    document.cookie = `${key}=${value};expires=${saveDate.toUTCString()}`;
+}
 
 
 
 
-
-// export default setCookie;
+export default setCookie;
